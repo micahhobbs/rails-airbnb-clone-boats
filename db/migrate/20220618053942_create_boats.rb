51d400cf4d0c21ce29623761009d858d697cd467
Micah Hobbs <micahthobbs@gmail.com>
2022-06-18 15:54:07 +1000

    Setup models and controllers

---
+++ db/migrate/20220618053942_create_boats.rb
@@ -0,0 +1,13 @@
+class CreateBoats < ActiveRecord::Migration[6.1]
+  def change
+    create_table :boats do |t|
+      t.string :name
+      t.string :type
+      t.text :description
+      t.decimal :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
