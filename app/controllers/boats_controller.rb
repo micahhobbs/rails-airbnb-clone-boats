ef416ba6f87a59d2672ffe4aa2e0983b1353f06a
Micah Hobbs <micahthobbs@gmail.com>
2022-06-21 20:29:46 +1000

    Added new boat form and button to home page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,4 +6,24 @@ class BoatsController < ApplicationController
   def show
     @boat = Boat.find(params[:id])
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save
+      redirect_to @boat
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :description, :category, :price)
+  end
 end
