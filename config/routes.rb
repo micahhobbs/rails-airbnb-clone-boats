d556a979ac1abd760c236a2c096563f0029f683d
Phil Tan <philcode40@gmail.com>
2022-06-23 21:02:42 +1000

    editted bookings links

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'boats#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boats
-  get '/bookings/my_bookings', to: 'bookings#my_bookings'
-  get '/bookings/bookings_on_my_boats', to: 'bookings#bookings_on_my_boats'
+  get '/my_bookings', to: 'bookings#my_bookings', as: 'my_bookings'
+  get '/bookings_on_my_boats', to: 'bookings#bookings_on_my_boats', as: 'bookings_on_my_boats'
   resources :boats do
     resources :bookings, only: [:new, :create]
   end
