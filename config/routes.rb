63620b0cad44888a56296d3af50d5c682c19730e
Phil Tan <philcode40@gmail.com>
2022-06-23 19:23:04 +1000

    changed seed file, updated bookings views to have tabs

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'boats#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boats
+  get '/bookings/my_bookings', to: 'bookings#my_bookings'
+  get '/bookings/bookings_on_my_boats', to: 'bookings#bookings_on_my_boats'
 end
