318230a37d644abc537e01aac59fd54eb5436af2
Phil Tan <philcode40@gmail.com>
2022-06-23 19:23:04 +1000

    changed seed file, updated bookings views to have tabs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+  def my_bookings
+    @my_bookings = Booking.all.select { |booking| booking.user_id == current_user.id }
+  end
+
+  def bookings_on_my_boats
+    my_boat_ids = current_user.boats.map {|boat| boat.id }
+    @bookings_on_my_boats = Booking.all.select { |booking| my_boat_ids.include? booking.boat_id }
+  end
 end
