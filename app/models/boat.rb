e74de324834500e61cf04002087b2f3c19d37139
Phil Tan <philcode40@gmail.com>
2022-06-25 09:41:28 +1000

    added search bar on boats index

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,16 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :category, :description, :price, presence: true
+
+  def self.search(query, user_id)
+    if query
+      query = query.downcase
+      Boat.where("LOWER(description) LIKE '%#{query}%'")
+          .or(Boat.where("LOWER(name) LIKE '%#{query}%'"))
+          .or(Boat.where("LOWER(category) LIKE '%#{query}%'"))
+          .and(Boat.where("user_id != ?", user_id))
+    else
+      Boat.where("user_id != ?", user_id)
+    end
+  end
 end
