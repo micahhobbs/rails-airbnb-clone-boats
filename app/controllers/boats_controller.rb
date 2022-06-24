8e680897be19160a68815868a14e63a5539c8bdf
Lachlan Munro <lachlangmunro@gmail.com>
2022-06-24 11:33:41 +1000

    Added functionality to delete a boat you own.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -39,6 +39,12 @@ class BoatsController < ApplicationController
     @boats = current_user.boats
   end
 
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to boats_path
+  end
+
   private
 
   def boat_params
